61c0e65c3e655d861dd41203b5722ee1a03b4e47
Erik Pukinskis <erik@snowedin.net>
2016-09-23 08:49:02 -0700

    finish back wall instructions

--- build_face_wall.js
+++ build_face_wall.js
@@ -2,8 +2,8 @@ var library = require("nrtv-library")(require)
 
 module.exports = library.export(
   "build-face-wall",
-  ["./steps", "./house_plan", "./dimension_text", "./some_materials"],
-  function(Steps, HousePlan, dimensionText, BASE_MATERIALS) {
+  ["./steps", "./house_plan", "./dimension_text", "./some_materials", "./face_wall"],
+  function(Steps, HousePlan, dimensionText, BASE_MATERIALS, faceWall) {
 
     function buildFaceWall(options, materials) {
 
@@ -62,27 +62,27 @@ module.exports = library.export(
         }
       )
 
-      if (options.joins == "right") {
+      var joinGaps = faceWall.getJoinGaps(options)
+
+      if (joinGaps.right != 0) {
         var alignmentStud = "leftmost"
         var firstSide = "left"
         var secondSide = "right"
         var firstInset = 2
         var secondInset = 1
-      } else if (options.joins == "left") {
+      } else if (joinGaps.left != 0) {
         var alignmentStud = "rightmost"
         var firstSide = "right"
         var secondSide = "left"
         var firstInset = 2
         var secondInset = 2
-      } else {
-        throw new Error("don't understand join")
       }
 
       steps.add("square interior",
         function(task) {
           task("position-interior", "Lay the interior (sanded) plywood on the framing, sanded face up, factory edge roughly lined up with the bottom track. If the plywood is bowed, put something heavy in the middle.") 
 
-          task("square-first-corner", "using a tri-square, make sure the plywood is vertically aligned with the "+alignmentStud+" stud and the bottom track, and ")
+          task("square-first-corner", "using a tri-square, make sure the plywood is vertically aligned with the "+alignmentStud+" stud and the bottom track")
 
           task("first-corner-screw", "Put a self-driving screw about <strong>"+dimensionText(1/2)+"</strong> from the bottom and about <strong>"+dimensionText(firstInset)+"</strong> from the "+firstSide)
 
@@ -90,11 +90,11 @@ module.exports = library.export(
 
           task("square-bottom-side", "Square the bottom "+secondSide+" corner the same way, and put a self-driving screw in <strong>"+dimensionText(1/2)+"</strong> from the bottom and <strong>"+dimensionText(secondInset)+"</strong> from the "+secondSide)
 
-          task("square-top-left", "Move the top left corner so that it is square with the stud and use your tri-square to check the top track is <strong>"+dimensionText(insideOverhang)+"</strong> from the edge of the plywood at every point.")
+          task("square-top-left", "Move the top left corner so that it is square with the stud and use your tri-square to check the top track is <strong>"+dimensionText(joinGaps.top)+"</strong> from the edge of the plywood at every point.")
 
-          task("screw-top-left", "Put a screw <strong>"+dimensionRound(insideOverhang+5/8)+"</strong> from the top and <strong>"+dimensionRound(firstInset)+"</strong> from the left")
+          task("screw-top-left", "Put a screw <strong>"+dimensionRound(joinGaps.top+5/8)+"</strong> from the top and <strong>"+dimensionRound(firstInset)+"</strong> from the left")
 
-          task("screw-top-left", "Put a screw <strong>"+dimensionRound(insideOverhang+5/8)+"</strong> from the top and <strong>"+dimensionRound(secondInset)+"</strong> from the right")
+          task("screw-top-left", "Put a screw <strong>"+dimensionRound(joinGaps.top+5/8)+"</strong> from the top and <strong>"+dimensionRound(secondInset)+"</strong> from the right")
 
         }
       )
@@ -118,15 +118,26 @@ module.exports = library.export(
 
       steps.add("align sheathing", function(task) {
 
+          task("lay-sheathing", "Lay the sheathing on the frame, with the factory edge toward the bottom.")
+
+          var bottomOverhang = options.bottomOverhang||0
+
+          var topOverhang = options.topOverhang||0 + joinGaps.top
+
+          task("square-first-sheathing-corner", "Align the sheathing so that the bottom edge is uniformly <strong>"+dimensionText(bottomOverhang)+"</strong> past the framing, and the "+secondSide+" edge is square with the plywood on the other side. Put a screw on the track line <strong>"+dimensionText(secondInset)+"</strong> from the "+secondSide)
+
+          task("square-mirror-sheathing-corner", "Align the top "+secondSide+" corner next, so that the top edge extends <strong>"+dimensionText(topOverhang)+"</strong> beyond the underlying framing. Put a screw on the track line <strong>"+dimensionText(secondInset)+"</strong> from the "+secondSide)
+
+          task("screw-opposite-side-sheaething", "Put screws on the top and bottom track lines <strong>"+dimensionText(firstInset)+"</strong> from the "+firstSide)
+
         }
-      ) 
+      )
 
-// align outside ply with inside ply on right and left edges
-// set with proper overhangs on top and bottom
-// left side:
-//   add a 2x3 stud on the right
-
-
+      steps.add("attach sheathing",
+        function(task) {
+          task("attach-sheathing", "Put screws on the stud and track lines every 10 inches as with the interior plywood")
+        }
+      )
 
       return steps
     }
