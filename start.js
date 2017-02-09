aa3e77ff29ba10c6b3dd1b8e0697a81fce296ce3
Erik Pukinskis <erik@snowedin.net>
2017-02-09 00:21:47 -0800

    Port to web-host

--- start.js
+++ start.js
@@ -1,1 +1,26 @@
-require("web-site").boot()
+var library = require("module-library")(require)
+
+library.using(
+  ["web-host", "./teensy-house-3", "./render-house-plan", "browser-bridge"],
+  function(host, teensyHouse, renderHousePlan, BrowserBridge) {
+
+    var house = teensyHouse()
+
+    host.onSite(prepareSite)
+
+    function prepareSite(site) {
+      site.addRoute(
+        "get",
+        "/house-plan/:view",
+        function(request, response) {
+          var bridge = new BrowserBridge()
+          var view = request.params.view || "side"
+          var page = renderHousePlan(bridge, house, {view: view})
+
+          bridge.sendPage(page)(request, response)
+        }
+      )
+    }
+
+  }
+)
